ebdea214764a8df54baf2583efe5b84fd48191ae
kongdejian <dejian.kdj@autonavi.com>
2017-04-28 11:43:57 +0800

    添加命令行

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # klvivian-gulp
 klvivian.com 自动化部署
+## kvgulp
+
+- **init** - 初始化kvgulp.
---
+++ config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kongxiaojian on 2017/4/28.
+ */
+var config = {
+    cwd: process.cwd(),
+    git: {
+        url:'https://github.com/kongxiaojian123/kongxiaojian123.github.com.git'
+    },
+    paths: {
+    },
+};
+
+module.exports = config;
